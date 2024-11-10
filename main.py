40542235d5f4f7cdee8cc156c46ee49bb062b9b9
Liam Weeks <liamweeks99@gmail.com>
2024-11-09 22:06:23 -0500

    got lower constraints working

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-from ast import Param
-from itertools import combinations
 
 """
 Create a Python(or Pseudo Python) function that solves how many non-negative integer solutions there
@@ -15,68 +13,33 @@ len(upper constraints)≤3. [Hint: Be careful of double constraints (eg. 3 ≤x2
 def choose(n, k):
     return math.comb(n, k)
 
-def negate(b):
-    """x_n ≤ b ==> x_n ≥ b"""
-    return b - 1
+def number_of_solutions(lower_constraints, upper_constraints, value, length):
+    cookies = value - sum(lower_constraints)
+    bars = length - 1
 
+    if bars < 0:
+        bars = 0
 
-def number_of_solutions(l, s):
-    # l: represents the number of terms in the equations
-    # s: represents the number of cookies to distribute x1 + x2 ... = s
-    global lower_constraints, upper_constraints
-    cookies = s
-    bars = l - 1
-    possible_equations_count = 0
+    sols_w_lower = choose(cookies + bars, bars)
 
-    ranges = [ [lower_constraints[i], s] for i in lower_constraints]
+    upper_constraints_count = len(upper_constraints)
 
-    for i in range(len(upper_constraints)):
-        index = upper_constraints[i][0]
-        max_val = upper_constraints[i][1]
-
-        ranges[index][1] = max_val
-
-    print(ranges)
-
-    unrestricted_total = choose(cookies + bars, bars)
-
-    # now each term is in the form a ≤ x_n ≤ b
-
-    for _range in ranges:
-        combinations = unrestricted_total - choose( cookies - negate(_range[1]) + bars, bars)
-        possible_equations_count += combinations
-
-    return possible_equations_count
-
-
-def test():
-    global lower_constraints, upper_constraints
-
-    count = 0
-
-    for a in range(7, 17):
-        for b in range(0, 17):
-            for c in range(0, 17):
-                for d in range(0, 17):
-                    if a + b + c +d == 17:
-                        count += 1
-    return count
-
-
-
-
-
-
+    if upper_constraints_count == 0:
+        return sols_w_lower
+    elif upper_constraints_count == 1:
+        pass
+    elif upper_constraints_count == 2:
+        pass
+    elif upper_constraints_count == 3:
+        pass
 
 if __name__ == "__main__":
-    LENGTH = 4
-    VALUE = 17
-
-    lower_constraints = [0, 0, 0, 0]
-    upper_constraints = [[1, 7]]
+    lower_constraints = [2, 3, 4, 2, 0]
+    upper_constraints = []
+    VALUE = 30
+    LENGTH = 5
 
 
     print(
-        number_of_solutions(LENGTH, VALUE),
-        test()
+        number_of_solutions(lower_constraints, upper_constraints, VALUE, LENGTH)
     )
